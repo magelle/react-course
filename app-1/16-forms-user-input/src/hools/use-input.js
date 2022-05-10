66af1403c2e3828e65ca8f454f751aa588310e9b
Maxime GELLE <maxime.gelle@sundayapp.com>
2022-05-11 00:09:08 +0200

    Section 16 - Working with user inputs - with reducer

--- app-1/16-forms-user-input/src/hools/use-input.js
+++ app-1/16-forms-user-input/src/hools/use-input.js
@@ -1,18 +1,29 @@
-import {useState} from "react";
+import {useReducer, useState} from "react";
+
+const initialInputState = {
+  value: '',
+  isTouched: false
+};
+const inputStateReducer = (state, action) => {
+  if (action.type === 'INPUT') return {...state, value: action.value}
+  if (action.type === 'BLUR') return {...state, isTouched: true}
+  if (action.type === 'RESET') return initialInputState
+  return initialInputState;
+}
+
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [{value, isTouched}, dispatch] = useReducer(inputStateReducer, initialInputState);
 
-  const valueIsValid = validateValue(enteredValue)
+  const valueIsValid = validateValue(value)
   const hasError = !valueIsValid && isTouched
 
-  const valueChangeHandler = (event) => setEnteredValue(event.target.value)
-  const inputBlurHandler = (event) => setIsTouched(true)
-  const reset = () => { setEnteredValue('') }
+  const valueChangeHandler = (event) => dispatch({type: 'INPUT', value: event.target.value})
+  const inputBlurHandler = (event) => dispatch({type: 'BLUR'})
+  const reset = () => dispatch({type: 'RESET'})
 
   return {
-    value: enteredValue,
+    value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
