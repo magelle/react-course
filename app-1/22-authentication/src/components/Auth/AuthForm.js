41634dc10c71c082367ecfc088284921d964ccdd
Maxime GELLE <maxime.gelle@sundayapp.com>
2022-05-22 12:27:17 +0200

    Section 22 - Authentication (on going)

---
+++ app-1/22-authentication/src/components/Auth/AuthForm.js
@@ -0,0 +1,88 @@
+import {useContext, useRef, useState} from 'react';
+
+import classes from './AuthForm.module.css';
+import AuthContext from "../store/AuthContext";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false)
+
+  const authContext = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    const url = isLogin
+      ? 'https://identitytoolkit.googleapis.com/v1/accounts:signIn?key=AIzaSyAB0nefHxOFlFS4vvkeR-cv2n-0ZMfo9wQ'
+      : 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAB0nefHxOFlFS4vvkeR-cv2n-0ZMfo9wQ';
+
+    setIsLoading(true);
+    fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => {
+      setIsLoading(false);
+      if (res.ok) {
+
+      } else {
+        return res.json().then(data => {
+          throw  new Error(data?.error?.message || 'Authentication failed!');
+        })
+      }
+    }).then(data => {
+      authContext.login(data.idToken)
+    })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' ref={emailInputRef} required/>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' ref={passwordInputRef} required/>
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request ...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
---
+++ app-1/22-authentication/src/components/Layout/MainNavigation.js
@@ -0,0 +1,32 @@
+import {Link} from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+import {useContext} from "react";
+import AuthContext from "../store/AuthContext";
+
+const MainNavigation = () => {
+  const authCtx = useContext(AuthContext)
+
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!authCtx.isLoggedIn && <li>
+            <Link to='/auth'>Login</Link>
+          </li>}
+          {authCtx.isLoggedIn && <li>
+            <Link to='/profile'>Profile</Link>
+          </li>}
+          {authCtx.isLoggedIn && <li>
+            <button>Logout</button>
+          </li>}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
